e43d705757fb0d12e2adc365c40ec60c42ab5d01
Luis <ernestoco.lc@gmail.com>
2018-11-04 12:10:42 -0300

    frontend

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <html>
 
 <head>
+    <title>Social Sport</title>
+    <!-- colos smarthphone -->
+    <meta name="theme-color" content="#ededed">
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <link type="text/css" rel="stylesheet" href="css/style.css" media="screen,projection" />
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -13,68 +17,53 @@
 </head>
 
 <body>
-    <nav class="orange darken-1">
-        <div class="nav-wrapper">
-            <a href="#!" class="brand-logo">Logo</a>
-            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">Javascript</a></li>
-                <li><a href="mobile.html">Mobile</a></li>
-            </ul>
-            <ul class="side-nav" id="mobile-demo">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">Javascript</a></li>
-                <li><a href="mobile.html">Mobile</a></li>
-            </ul>
-        </div>
-    </nav>
-
     <div class="row">
         <div class="col s10 offset-s1">
-            <div class="card">
+            <div class="card cardlogin">
                 <div class="row">
-                    <div class="col s12 m6 l6 center">
-                        <p>Inscribí tu partido acá</p>
-                    </div>
-                    
-                    <div class="col s12 m6 l6 center">
-                        <p>Segundo partido</p>
-                    </div>
+                    <form class="col s12">
+                        <div class="row">
+                            <div class="col s12">
+                                <center>
+                                    <img src="img/user.png" class="useracount">
+                                </center>
+                            </div>
+                            
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">email</i>
+                                <input id="icon_prefix" type="email" class="validate">
+                                <label for="icon_prefix">Email</label>
+                            </div>
+                            <div class="input-field col s12">
+                                <i class="material-icons prefix">vpn_key</i>
+                                <input id="icon_telephone" type="password" class="validate">
+                                <label for="icon_telephone">Contraseña</label>
+                            </div>
+
+                            <div class="col s12">
+                                <center>
+                                    <a class="blue btn" href="dashboard.html">Ingresar</a>
+                                </center>
+                                <br>
+                                <center>
+                                    <a class="blue-text" href="registrar.html">Registrar</a>
+                                </center>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 
-    <footer class="page-footer orange darken-1">
-        <div class="container">
-            <div class="row">
-                <div class="col l6 s12">
-                    <h5 class="white-text">Footer Content</h5>
-                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-                </div>
-                <div class="col l4 offset-l2 s12">
-                    <h5 class="white-text">Links</h5>
-                    <ul>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+    <footer class="page-footer transparent">
         <div class="footer-copyright">
-            <div class="container">
-                © 2014 Copyright Text
-                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <div class="container grey-text">
+                Social Sports
             </div>
         </div>
     </footer>
-
-
+    
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
